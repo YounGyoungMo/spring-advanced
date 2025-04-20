5c2c6b442d9b04a0307644941b7cf53109909379
YounGyoungMo <gyoungmo2011@naver.com>
2025-04-20 20:22:32 +0900

    Lv 2 : N+1 문제

--- src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
@@ -2,6 +2,8 @@ package org.example.expert.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
